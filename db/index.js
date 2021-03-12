ff58b9230e5d07c910529487004692df184e23ad
Jose H Gonzalez <40965405+Hilarioo@users.noreply.github.com>
2021-03-12 01:00:57 -0800

    self-signed certificate

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
+const pgp = require("pg-promise")();
+
 // self-signed certificate connection
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
-
-const pgp = require("pg-promise")();
 const connection = pgp(process.env.DATABASE_URL);
 
 module.exports = connection;
