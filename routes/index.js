6bfa32f27f9be08acbd1d42fd962a78c346b6210
Jose H Gonzalez <40965405+Hilarioo@users.noreply.github.com>
2021-04-07 18:36:28 -0700

    signup & signin structure with bootstrap

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+/* GET signup page. */
+router.get("/signup", function (req, res, next) {
+  res.render("signup", { title: "Signup" });
+});
+
+/* GET signin page. */
+router.get("/signin", function (req, res, next) {
+  res.render("signin", { title: "Signin" });
 });
 
 module.exports = router;
