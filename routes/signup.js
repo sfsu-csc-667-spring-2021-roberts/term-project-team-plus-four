4a79e9331504c841af8e6e40bc9c756652c12966
John Freirez <jfreirez@mail.sfsu.edu>
2021-04-07 17:09:19 -0700

    playing around with pug, still not used to syntax

--- routes/signup.js
+++ routes/signup.js
@@ -1,9 +1,11 @@
 var express = require('express');
+const app = require('../app');
+const { $pool } = require('../db');
 var router = express.Router();
 
 /* GET signup page. */
 router.get('/', function(req, res, next) {
-  res.render('signup', { title: 'SignUp' });
+  res.render('signup', { title: 'Singup' });
 });
 
 module.exports = router;
