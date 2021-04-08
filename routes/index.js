1cbd78467cb81b0ac2822ddde357d32e7965825c
Jose H Gonzalez <40965405+Hilarioo@users.noreply.github.com>
2021-04-08 05:16:42 -0700

    routes for other pages added

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,37 @@ router.get("/", function (req, res, next) {
 
 /* GET signup page. */
 router.get("/signup", function (req, res, next) {
-  res.render("signup", { title: "Signup" });
+  res.render("signup", { title: "Uno Game | Signup" });
 });
 
 /* GET signin page. */
 router.get("/signin", function (req, res, next) {
-  res.render("signin", { title: "Signin" });
+  res.render("signin", { title: "Uno Game | Signin" });
+});
+
+/* GET dashboard page. */
+router.get("/dashboard", function (req, res, next) {
+  res.render("dashboard", { title: "Uno Game | Dashboard" });
+});
+
+/* GET new game (lobby) page. */
+router.get("/lobby", function (req, res, next) {
+  res.render("lobby", { title: "Uno Game | Lobby" });
+});
+
+/* GET resume game page. */
+router.get("/resume-game", function (req, res, next) {
+  res.render("resume-game", { title: "Uno Game | Resume Game" });
+});
+
+/* GET join game page. */
+router.get("/join-game", function (req, res, next) {
+  res.render("join-game", { title: "Uno Game | Join Game" });
+});
+
+/* GET game page. */
+router.get("/game", function (req, res, next) {
+  res.render("game", { title: "Uno Game" });
 });
 
 module.exports = router;
