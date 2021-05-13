d75bda9f4a7cf55923797c1f48f3b6dfc11ee75d
Jose H Gonzalez <40965405+Hilarioo@users.noreply.github.com>
2021-05-13 00:03:21 -0700

    Update join-game.js

---
+++ public/scripts/join-game.js
@@ -0,0 +1,51 @@
+/**
+ * File: join-game.js
+ * Location: This js file is included as a script within /views/join-game.pug
+ * Purpose: Allows logged in user to enter a new game with a unique code.
+ * Checks to see if game has open availability, if not game full message is prompted
+ *
+ */
+
+let code;
+
+/*========================== [Event Listeners] ==========================*/
+/**
+ * Purpose: Updates unique code variable every time user changes input
+ */
+document.getElementById("code").addEventListener("change", (e) => {
+  code = e.target.value.toUpperCase();
+  console.log("[DEBUG] code: " + code);
+});
+
+document.getElementById("join-game-btn").addEventListener("click", (e) => {
+  validInput(); //validates entry and sends user to game if game slots open
+});
+
+/*==========================  [Helper Fucntions]  ==========================*/
+/**
+ * Purpose: Checks to see if the input is of length 10 || not empty
+ */
+const validInput = () => {
+  if (code === undefined || code.length !== 10) {
+    alert("[Error] Game Code must be ten characters long");
+  } else {
+    if (openSlots()) {
+      // TODO: add game to users open games
+
+      // send user to game
+      window.location.href = `/game/${code}`;
+    } else alert("Sorry game is now full");
+  }
+};
+
+/**
+ * Purpose: Checks to see if the game is in DB && open slots available
+ */
+const openSlots = () => {
+  // TODO: grab game from open games DB
+  let game = { id: 0, code: "0123456789", players: 9 };
+
+  console.log(game.players);
+  if (game.players < 10 && game.code == code) return true;
+  return false;
+};
