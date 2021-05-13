8735f6662748fc306cbd1d3568e6964a61145ed1
Jose H Gonzalez <40965405+Hilarioo@users.noreply.github.com>
2021-05-12 20:12:50 -0700

    frontend game complete

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,7 +1,6 @@
-// grabs card names from "/content/svg/deck" and place them into deck object
-
+// deck is an initial array of stored card names retrieved from /contents/svg/deck
 let deck = [
-  // [TODO] read all files within /svg/deck and place it in a string array
+  // Blue Cards
   "blue-00.svg",
   "blue-01.svg",
   "blue-02.svg",
@@ -15,7 +14,7 @@ let deck = [
   "blue-draw2.svg",
   "blue-reverse.svg",
   "blue-skip.svg",
-
+  // Green Cards
   "green-00.svg",
   "green-01.svg",
   "green-02.svg",
@@ -29,7 +28,7 @@ let deck = [
   "green-draw2.svg",
   "green-reverse.svg",
   "green-skip.svg",
-
+  // Red Cards
   "red-00.svg",
   "red-01.svg",
   "red-02.svg",
@@ -43,7 +42,7 @@ let deck = [
   "red-draw2.svg",
   "red-reverse.svg",
   "red-skip.svg",
-
+  // Yellow Cards
   "yellow-00.svg",
   "yellow-01.svg",
   "yellow-02.svg",
@@ -57,15 +56,16 @@ let deck = [
   "yellow-draw2.svg",
   "yellow-reverse.svg",
   "yellow-skip.svg",
-
+  // Wild Cards
   "wild-draw4.svg",
   "wild-wild.svg",
 ];
 
+// TODO: Fill players object with players in current game
 let players = [
   {
-    id: 0, // [TODO] user id from db
-    name: "Bob Lee", // [TODO] user name from db
+    id: 0,
+    name: "Bob Lee",
     cards: [],
   },
   {
@@ -85,18 +85,74 @@ let players = [
   },
 ];
 
-let grabDeck = ["blue-00.svg", "blue-01.svg", "blue-03.svg", "blue-04.svg"];
-let selectedCard = null; // card user chooses to play
-let activeUser = null; // user who start the game
-let topPlacedCard = { color: null, number: null };
+let usedDeck = []; // once a user plays a card, it goes here
+let selectedCard = null; // stores the card active user chooses to add to deck
+let activeUserIndex = 0;
+let activeUser = players[activeUserIndex]; // stores active user
+let topPlacedCard = { color: null, number: null }; // top card in the usedeck pile
 
-// add code from URL to UI
-let gameCode = window.location.toString().split("game/")[1];
-let codeStr = document.createElement("p");
-codeStr.appendChild(document.createTextNode(`Game Code: ${gameCode}`));
-document.querySelector(".header-game").appendChild(codeStr);
+/*========================== [Event Listeners] ==========================*/
+/**
+ * Purpose: Waits on user to click "Start Game" button
+ */
+document.getElementById("game-start-btn").addEventListener("click", (e) => {
+  e.preventDefault;
+  startGame(); // paints elements and starts game
+});
 
-// creates opponent element and adds it to the UI
+/**
+ * Purpose: Waits on user to click "Grab Card" button
+ */
+document.querySelector("#grabCard").addEventListener("click", (e) => {
+  e.preventDefault();
+  let newCard = grabNewCard();
+  console.log("new card: " + newCard);
+  activeUser.cards.push(newCard); // assigns new card to user's deck
+
+  // TODO: Server-Side (update active users deck)
+
+  waitTurn(true); // next user
+});
+
+/**
+ * Purpose: Waits on user to click "Place Card" button
+ *
+ * Functionality:
+ * #1 Checks if card has been selected
+ * #2 Validates the chosen card to play
+ * #3 moves onto next player
+ */
+document.querySelector("#placeCard").addEventListener("click", (e) => {
+  // Card selected ?
+  if (selectedCard === null)
+    return alert("Please select a card from your pile or grab a card");
+
+  // Validates chosen card
+  if (validCard(selectedCard) || topPlacedCard.color === null) {
+    // removes card from UI
+    placeCard(selectedCard);
+    // removes card from users deck
+    let newDeck = activeUser.cards.filter((card) => card !== selectedCard);
+    activeUser.cards = newDeck;
+  } else
+    return alert(
+      `Invalid Card: Card must be ${topPlacedCard.color} or be a ${topPlacedCard.number}`
+    );
+
+  // TODO: Server-Side (update active users deck)
+
+  waitTurn(true); // next user
+});
+
+/*==========================  [Element Creators]  ==========================*/
+
+/**
+ * Purpose: Creates opponent element and adds it to the UI
+ *
+ * @param {integer} id Opponent's id
+ * @param {string} name Opponents name
+ * @param {integer} totalCards Opponents total number of cards
+ */
 const opponentElement = (id, name, totalCards) => {
   let opponent = document.createElement("div");
   let opponentImg = document.createElement("img");
@@ -121,55 +177,14 @@ const opponentElement = (id, name, totalCards) => {
   document.querySelector(".middle-game").appendChild(opponent);
 };
 
-// Adds card to user's pile
-document.querySelector("#grabCard").addEventListener("click", (e) => {
-  e.preventDefault();
-  // Client-Side (Show card to user)
-  console.log("[DEBUG] Card Added");
+/*==========================  [Helper Fucntions]  ==========================*/
 
-  // Server-Side (Add card to DB)
-
-  // Move onto the next user's turn
-});
-
-// waiting on user to click "Start Game" button
-document.getElementById("game-start-btn").addEventListener("click", (e) => {
-  e.preventDefault;
-  startGame();
-});
-
-// Play selected card onto the pile
-document.querySelector("#placeCard").addEventListener("click", (e) => {
-  // Client-Side (Remove card from pile)
-  if (selectedCard === null)
-    return alert("Please select a card from your pile or grab a card");
-  let newDeck = players[0].cards.filter((card) => card !== selectedCard);
-  players[0].cards = newDeck;
-
-  console.log("[DEBUG] Card Placed: " + players[0].cards);
-
-  // validates if the selected card can be placed
-  if (validCard(selectedCard) || topPlacedCard.color === null) {
-    // removes card from UI & Updates the Place Cards
-    placeCard(selectedCard);
-  } else
-    return alert(
-      `Invalid Card: Card must be ${topPlacedCard.color} or be a ${topPlacedCard.number}`
-    );
-
-  // Server-Side (Add Card to DB)
-
-  // Move onto the next user's turn
-  waitTurn(true);
-});
-
-const startGame = () => {
-  // shuffle the deck
-  let myCards = document.querySelector(".my-cards");
-  let card;
+/**
+ * Purpose: Shuffles deck array when called
+ */
+const shuffleDeck = () => {
   let tmpValue;
   let random;
-  let num = 7;
 
   for (let i = 0; i < deck.length; i++) {
     random = Math.floor(Math.random() * i);
@@ -178,50 +193,33 @@ const startGame = () => {
     deck[i] = deck[random];
     deck[random] = tmpValue;
   }
-
-  console.log("[DEBUG] shuffled deck: " + players.length);
-
-  // assign cards
-  for (let i = 0; i < players.length; i++) {
-    for (let j = 0; j < num; j++) {
-      players[i].cards.push(deck.pop());
-    }
-  }
-
-  // add opponent elements to UI
-  for (let i = 1; i < players.length; i++) {
-    opponentElement(players[i].id, players[i].name, players[i].cards.length);
-  }
-
-  // add user cards to the UI
-  for (let i = 0; i < num; i++) {
-    card = document.createElement("img");
-    card.className = "cards";
-    card.id = players[0].cards[i];
-    card.src = `../content/svg/deck/${players[0].cards[i]}`;
-    myCards.appendChild(card);
-  }
-
-  // Updates selected card
-  let cards = document.getElementsByClassName("cards");
-  Array.from(cards).forEach((card) => {
-    card.addEventListener("click", (e) => {
-      console.log(card.id);
-      console.log("[DEBUG] Selected Card: " + e.target.id);
-      selectedCard = e.target.id;
-    });
-  });
-
-  // assign active user
-  activeUser = players[0].id;
-  // removes overlay and modal
-  document.getElementById("game-start").remove();
-  document.getElementById("game-start-modal").remove();
 };
 
-const validCard = (selectedCard) => {
+/**
+ * Purpose: Grabs available cards from usedDeck if deck array empty.
+ * Then assigns random card to user requesting it.
+ *
+ * @return {string} deck.pop() last card from the deck
+ */
+const grabNewCard = () => {
+  if (deck.length == 0) {
+    deck = usedDeck;
+    shuffleDeck();
+    usedDeck = [];
+  }
+  return deck.pop();
+};
+
+/**
+ * Purpose: Grabs available cards from usedDeck if deck array empty.
+ * Then assigns random card to user requesting it.
+ *
+ * @param {string} card active user selected card which needs validation
+ * @return {boolean} true: card cad be placed onto deck, fasle: card cant be placed
+ */
+const validCard = (card) => {
   // grab top card from Placed Deck
-  let split = selectedCard.split("-");
+  let split = card.split("-");
   if (
     split[0] === "wild" ||
     split[0] === topPlacedCard.color ||
@@ -231,12 +229,18 @@ const validCard = (selectedCard) => {
   return false;
 };
 
-const placeCard = (selectedCard) => {
+/**
+ * Purpose: Makes user choose new color if card passed is a wild card,
+ * then places card onto the UI of used Deck and removes card form active users deck
+ *
+ * @param {string} card user chosen card which must be removed from th
+ */
+const placeCard = (card) => {
   let colorSelected = null;
   let split;
 
   // makes user choose new color
-  if (selectedCard.includes("wild")) {
+  if (card.includes("wild")) {
     do {
       colorSelected = prompt("Choose new color (blue, green, blue, yellow): ");
     } while (
@@ -248,15 +252,13 @@ const placeCard = (selectedCard) => {
   }
 
   // removes card from UI
-  document.getElementById(selectedCard).remove();
+  document.getElementById(card).remove();
 
   // updates place deck to show the new card on top
-  document.getElementById(
-    "placedDeck"
-  ).src = `../content/svg/deck/${selectedCard}`;
+  document.getElementById("placedDeck").src = `../content/svg/deck/${card}`;
 
   // updates topPlacedCard
-  split = selectedCard.split("-");
+  split = card.split("-");
   topPlacedCard.color = colorSelected !== null ? colorSelected : split[0];
   topPlacedCard.number = split[1].slice(0, split[1].length - 4);
 
@@ -264,15 +266,24 @@ const placeCard = (selectedCard) => {
   selectedCard = null;
 };
 
-// if its not the user's turn, show waiting screen
+/**
+ * Purpose: Creates an overlay for the user logged in if its not their turn. Otherwise content clickable
+ *
+ * @param {boolean} wait True: user must have a waiting overlay, False: user is currently active, no overlay
+ */
 const waitTurn = (wait) => {
+  // updates active user
+  if (activeUserIndex === activeUser.length - 1) activeUserIndex = 0;
+  else activeUserIndex++;
+  activeUser = players[activeUserIndex];
+
   // create modal message
   let msg = document.createElement("div");
   let msgHeader = document.createElement("h5");
   let msgBody = document.createElement("p");
   msg.className = "waiting-modal";
   msgHeader.appendChild(
-    document.createTextNode(`It's now ${players[1].name}'s turn`)
+    document.createTextNode(`It's now ${activeUser.name}'s turn`)
   );
   msgBody.appendChild(
     document.createTextNode("In the meantime, feel free to chat....")
@@ -296,12 +307,63 @@ const waitTurn = (wait) => {
   }
 };
 
-/* shuffles the deck 
-  if deck empty:
-    grab them from the used card array,
-  else:
-    no card can be issued
+/**
+ * Purpose: Initiates game once user clicks on "Start Game" button
+ *
+ * Functionality:
+ * #1 Add unique game code to UI
+ * #2 Shuffles initial deck
+ * #3 Assigns cards
+ * #4 Paints UI according to amount of users and cards from their personal decks
+ * #5 Removes waiting overlay
+ */
+const startGame = () => {
+  let myCards = document.querySelector(".my-cards");
+  let card;
+  let num = 7;
 
-*/
+  // add's unique game code to UI
+  let gameCode = window.location.toString().split("game/")[1];
+  let codeStr = document.createElement("p");
+  codeStr.appendChild(document.createTextNode(`Game Code: ${gameCode}`));
+  document.querySelector(".header-game").appendChild(codeStr);
 
-// add the card to the user's deck
+  // shuffles deck before assigning cards
+  shuffleDeck();
+  console.log("[DEBUG] shuffled deck: " + players.length);
+
+  // assigns cards
+  for (let i = 0; i < players.length; i++) {
+    for (let j = 0; j < num; j++) {
+      players[i].cards.push(deck.pop());
+    }
+  }
+
+  // adds opponent elements to UI
+  for (let i = 1; i < players.length; i++) {
+    opponentElement(players[i].id, players[i].name, players[i].cards.length);
+  }
+
+  // adds user cards to the UI
+  for (let i = 0; i < num; i++) {
+    card = document.createElement("img");
+    card.className = "cards";
+    card.id = players[0].cards[i];
+    card.src = `../content/svg/deck/${players[0].cards[i]}`;
+    myCards.appendChild(card);
+  }
+
+  // adds event listener to user's cards
+  let cards = document.getElementsByClassName("cards");
+  Array.from(cards).forEach((card) => {
+    card.addEventListener("click", (e) => {
+      console.log(card.id);
+      console.log("[DEBUG] Selected Card: " + e.target.id);
+      selectedCard = e.target.id;
+    });
+  });
+
+  // removes overlay and modal
+  document.getElementById("game-start").remove();
+  document.getElementById("game-start-modal").remove();
+};
